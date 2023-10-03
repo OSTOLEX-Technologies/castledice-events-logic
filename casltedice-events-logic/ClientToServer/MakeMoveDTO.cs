461dc55571c1f1056892de7eab00781d742e25da
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-10-04 00:22:00 +0300

    Added classes for client to server messages.

---
+++ casltedice-events-logic/ClientToServer/MakeMoveDTO.cs
@@ -0,0 +1,14 @@
+﻿using castledice_game_data_logic.MovesData;
+
+namespace casltedice_events_logic.ClientToServer;
+
+[Serializable]
+public class MakeMoveDTO : ClientToServerDTO
+{
+    public MoveData MoveData { get; set; }
+    
+    public MakeMoveDTO(MoveData moveData, string verificationKey) : base(verificationKey)
+    {
+        MoveData = moveData;
+    }
+}
---
+++ casltedice-events-logic/ClientToServer/RequestGameDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace casltedice_events_logic.ClientToServer;
+
+[Serializable]
+public class RequestGameDTO : ClientToServerDTO
+{
+    public int PlayerId { get; set; }
+
+    public RequestGameDTO(string verificationKey, int playerId) : base(verificationKey)
+    {
+        PlayerId = playerId;
+    }
+}
