ce79ca9a1c8856780444379e5979368c2f5f9846
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-10-09 12:44:19 +0300

    Wrote MatchFoundDTO.

---
+++ casltedice-events-logic/ServerToClient/MatchFoundDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace casltedice_events_logic.ServerToClient;
+
+public class MatchFoundDTO
+{
+    public List<int> PlayerIds { get; set; }
+    public string GameUUID { get; set; }
+
+    public MatchFoundDTO(List<int> playerIds, string gameUuid)
+    {
+        PlayerIds = playerIds;
+        GameUUID = gameUuid;
+    }
+}
