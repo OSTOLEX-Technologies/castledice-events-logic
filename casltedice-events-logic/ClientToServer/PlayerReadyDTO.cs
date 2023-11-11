39ff0892b417eeb950bb1bd46b969162d9fbe842
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-11-11 21:32:17 +0200

    Sealed PlayerReadyDTO.

--- casltedice-events-logic/ClientToServer/PlayerReadyDTO.cs
+++ casltedice-events-logic/ClientToServer/PlayerReadyDTO.cs
@@ -1,6 +1,6 @@
 ﻿namespace casltedice_events_logic.ClientToServer;
 
-public class PlayerReadyDTO : ClientToServerDTO
+public sealed class PlayerReadyDTO : ClientToServerDTO
 {
     public PlayerReadyDTO(string verificationKey) : base(verificationKey)
     {
